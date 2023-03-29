4a2c84632c6578bcaa68bd4c9e26688b6176aecd
Jordan Jenkins <jenkins.587@gmail.com>
2023-03-29 12:29:31 -0400

    Update sensor_app.py

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -17,12 +17,12 @@ print("Loaded Records: {}".format(len(data)))
 # Module 2 code here:
 house_info = HouseInfo(data)
 test_area = 1
-recs = house_info.get_data_by_area(field="id", rec_area=test_area)
+recs = house_info.get_data_by_area("id", rec_area=test_area)
 print("\nHouse sensor records for area {} = {}".format(test_area, len(recs)))
 
 # Module 3 code here:
 test_date = datetime.strptime("5/9/20", "%m/%d/%y")
-recs = house_info.get_data_by_date(field="id", rec_date=test_date)
+recs = house_info.get_data_by_date("id", rec_date=test_date)
 print("\nHouse sensor records for date: {} = {}".format(test_date.strftime("%m/%d/%y"), len(recs)))
 # Module 4 code here:
 
